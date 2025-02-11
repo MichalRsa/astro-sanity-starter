e8f368deb0e7b12a3fa7947b288c79b7c79e21c4
MichalRsa <michalrosa17@gmail.com>
2025-02-11 09:34:27 +0100

    adds astro sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [sitemap()]
+  integrations: [
+    sitemap(),
+    sanity({
+      projectId: "0azllx90",
+      dataset: "production",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    }),
+    react(),
+  ],
 });
+
